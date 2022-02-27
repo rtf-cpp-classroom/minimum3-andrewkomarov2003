4c0ce7127a1db35818565de6c5f39cc647e4d4e8
andrewkomarov2003 <94227230+andrewkomarov2003@users.noreply.github.com>
2022-02-27 15:45:43 +0200

    Komarov RS-01

--- main.cpp
+++ main.cpp
@@ -3,9 +3,20 @@
 
 int main()
 {
-	//TODO: if you want to test your functions, write your code here
 	
+	float a, b, c, result;
 	
-	
+	cout << "Please enter three numbers:" << endl << endl << "Number 1: ";
+	cin >> a;
+	cout << "Number 2: ";
+	cin >> b;
+	cout << "Number 3: ";
+	cin >> c;
+	cout << endl;
+
+	result = my_func(a, b, c);
+
+	cout << "The smallest number: " << result << endl;
+
 	return 0;
 }
