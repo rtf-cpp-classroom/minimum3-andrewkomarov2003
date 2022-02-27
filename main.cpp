6e51200f9889386b5a3ae0cfe15ae42b9fee3e87
andrewkomarov2003 <94227230+andrewkomarov2003@users.noreply.github.com>
2022-02-27 16:31:28 +0200

    test

--- main.cpp
+++ main.cpp
@@ -5,9 +5,25 @@
 
 using namespace std;
 
+float my_func(float a, float b, float c)
+{
+
+	float result, num1;
+
+	if (a >= b) {
+		num1 = b;
+	}
+	else num1 = a;
+	if (num1 >= c) {
+		result = c;
+	}
+	else result = num1;
+
+	return result;
+}
+
 int main()
 {
-	
 	float a, b, c, result;
 	
 	cout << "Please enter three numbers:" << endl << endl << "Number 1: ";
