3632ddce0c13d02236d4c6d2a3ea241022d2d36e
andrewkomarov2003 <94227230+andrewkomarov2003@users.noreply.github.com>
2022-02-27 15:52:37 +0200

    Komarov RS-01

--- functions.h
+++ functions.h
@@ -19,3 +19,22 @@ float my_func(float a, float b, float c)
 
 	return result;
 }
+
+int main()
+{
+	float a, b, c, result;
+	
+	cout << "Please enter three numbers:" << endl << endl << "Number 1: ";
+	cin >> a;
+	cout << "Number 2: ";
+	cin >> b;
+	cout << "Number 3: ";
+	cin >> c;
+	cout << endl;
+
+	result = my_func(a, b, c);
+
+	cout << "The smallest number: " << result << endl;
+
+	return 0;
+}
