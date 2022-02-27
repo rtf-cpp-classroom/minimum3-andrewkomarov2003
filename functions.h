7cc2b3d0662928e9ad83dfe408bc6b3a8b439288
andrewkomarov2003 <94227230+andrewkomarov2003@users.noreply.github.com>
2022-02-27 16:12:25 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,22 +1,5 @@
-#include <iostream>
-#include <string>
+#pragma once
 
-using namespace std;
 
-float my_func(float a, float b, float c)
-{
 
-	float result, num1;
-
-	if (a >= b) {
-		num1 = b;
-	}
-	else num1 = a;
-	if (num1 >= c) {
-		result = c;
-	}
-	else result = num1;
-
-	return result;
-}
-
+float findMinValue(float n1, float n2, float n3);
